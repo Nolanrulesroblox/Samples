f7ea8362952b247557c1b7a0fe2b1b1da6778a16
Nolan <54533608+Nolanrulesroblox@users.noreply.github.com>
2023-01-09 03:43:54 -0800

    Update tools.py
    making all future database queries as a class, with functions to get results/error/length
    
    for in the future, if we need to convert into a different datastore (i.e. mongoDB, PSQL, cassandra), we just have to changes the queries and class, not the whole script.
    
    changes how ```column``` is handled, preventing duplicating the same code over and over again. (can lead to errors if not both are the same)

--- python/tools.py
+++ python/tools.py
@@ -1,58 +1,43 @@
-import mysql.connector
-def query(query: str,params: list,handle,column = True):
-    handle.commit()
-    #to keep mysql connection up to date
-    err = 0
-    #if err code 0, nothing went wrong
-    result = []
-    if(column):
-      #if the script needs column names (like to return to a json file)
+import mysql
+class DatabaseQueryMysql():
+    def query(query: str,params: list,handle,column = True):
+        handle.commit()
+        err = 0
+        result = ''
         try:
             mycursor = handle.cursor()
-            #create a mysql cursor
             mycursor.execute(query,params)
-            #execute the query
-            columns = mycursor.description 
-            result = [{columns[index][0]:column for index, column in enumerate(value)} for value in mycursor.fetchall()]
-            returnData = {
-                "result":result, #result will be a list
-                "length":len(result), #returns an int of how many results
-                "error":err, #error code, anything but 0 is a MYSQL error
-                "affectedRows":mycursor.rowcount #returns an int of how many results were updated, -1 means an error has occurred
+            if column:
+                columns = mycursor.description
+                result = [{columns[index][0]:column for index, column in enumerate(value)} for value in mycursor.fetchall()]
+            else:
+                result = mycursor.fetchall()
+            returndata = {
+                "result":result,
+                "length":len(result),
+                "error":int(err),
+                "affectedRows":mycursor.rowcount
             }
-            #the returnData is all the same for each try/except
             handle.commit()
-            return returnData
+            return returndata
         except mysql.connector.Error as err:
-            #if any error, return the error
             err = err
-            returnData = {
+            returndata = {
                 "result":result,
                 "length":len(result),
                 "error":int(err.errno),
                 "affectedRows":mycursor.rowcount
             }
-        return returnData
-    else:
-        try:
-            mycursor = handle.cursor()
-            mycursor.execute(query,params)
-            columns = mycursor.description 
-            result = mycursor.fetchall()
-            returnData = {
-                "result":result,
-                "length":len(result),
-                "error":err,
-                "affectedRows":mycursor.rowcount
-            }
             handle.commit()
-            return returnData
-        except mysql.connector.Error as err:
-            err = err
-            returnData = {
-                "result":result,
-                "length":len(result),
-                "error":int(err.errno),
-                "affectedRows":mycursor.rowcount
-            }
-            return returnData
+            return returndata
+    def result(query):
+        #just returns the result.
+        return query['result']
+    def error(query):
+        if int(query['error']) != 0:
+            return int(query['error'])
+        return False
+    def changedRows(query):
+        return query['affectedRows']
+    def length(query):
+        return query['length']
